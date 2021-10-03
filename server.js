5f815cb2437b1d7c77d8f6adc992e9c19869358e
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-03 13:03:18 -0400

    added query statements

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mysql = require('mysql2');
 
@@ -6,30 +5,55 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Brenman77',
-      database: 'election'
+const db = mysql.createConnection({
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Brenman77',
+        database: 'election'
     },
     console.log('Connected to the election database.')
-  );
+);
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
 
-//default response for any other requests (Not found)
-app.use((req, res) => {
-    res.status(404).end();
-})
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+//delete a candidate
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 
 

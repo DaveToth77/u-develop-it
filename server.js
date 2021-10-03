af7f2feef494886061f09885ff818fa5739a5cbd
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-03 12:42:19 -0400

    created database route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,23 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Brenman77',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other requests (Not found)
 app.use((req, res) => {

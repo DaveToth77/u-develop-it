3f2de5afcac04cd59fd6185ab03d079c437be15e
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-04 18:02:10 -0400

    added party name

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const db = mysql.createConnection({
 
 //get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -43,8 +48,14 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
+         
 
     db.query(sql, params, (err, row) => {
         if (err) {
